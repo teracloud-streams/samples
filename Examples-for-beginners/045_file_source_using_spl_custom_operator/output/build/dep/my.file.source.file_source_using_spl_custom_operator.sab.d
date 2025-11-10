7b0498e7a6d6bc60c459a1f8aa7558ad0765efbe
mbranson <mbranson@dev3-rl9.teracloud.com>
2025-11-10 12:19:42 -0600

    Example for beginners 045 added

--- Examples-for-beginners/045_file_source_using_spl_custom_operator/output/build/dep/my.file.source.file_source_using_spl_custom_operator.sab.d
+++
@@ -1,3 +0,0 @@
-my.file.source.file_source_using_spl_custom_operator.sab: \
-  /homes/mbranson/streams/7.2.0.1/toolkits/spl/toolkit.xml \
-  /homes/mbranson/samples-git-space/samples/Examples-for-beginners/045_file_source_using_spl_custom_operator/data/stock_report.dat
